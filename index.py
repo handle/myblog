74b7ddcae509f859ddfe3d5cad4ce40cede87891
贷成 <haidong.yanhd@alibaba-inc.com>
2015-07-18 23:24:58 +0800

    rmeove idea

--- index.py
+++ index.py
@@ -59,4 +59,4 @@ def contextUtil():
     return dict(renderNav=renderNav,renderPager=renderPager)
 
 if __name__ == "__main__":
-    app.run(host='192.168.1.101',port=8080,debug=True)
+    app.run(host='127.0.0.1',port=8080,debug=True)
